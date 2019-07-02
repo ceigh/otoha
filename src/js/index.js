3b98aa1d3a8d4ae6a37d21ab0710aac72beb1bcc
Artjom Löbsack <ceigh@pm.me>
2019-07-02 16:28:07 +0500

    feat: Add vk auth

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,14 @@
 import '../css/index.css';
+import cookie from './cookie';
+
+// If change, don't forget to also change on vk.com/dev
+const baseUrl = 'http://localhost:9090';
+const authUrl = 'https://oauth.vk.com/authorize?client_id' +
+  `=7041394&display=page&scope=offline&redirect_uri=${baseUrl}&response_type=token`;
+const url = location.href;
+const urlData = getJsonFromUrl(url.substr(url.search('#')));
 
 const input = document.querySelector('#search-block input');
-
 const selectOnClick = el => {
   el.addEventListener('click', () => {
     el.focus();
@@ -10,3 +17,31 @@ const selectOnClick = el => {
 };
 
 selectOnClick(input);
+
+if ('access_token' in urlData) {
+  cookie.set('token', urlData.access_token);
+  location.replace(baseUrl);
+}
+
+input.onchange = () => {
+  const token = cookie.get('token');
+
+  if (token) {
+    console.log(token);
+  } else {
+    location.replace(authUrl);
+  }
+};
+
+
+function getJsonFromUrl(url) {
+  if (!url) url = location.href;
+  const query = url.substr(1);
+  const result = {};
+
+  query.split('&').forEach(function(part) {
+    const item = part.split('=');
+    result[item[0]] = decodeURIComponent(item[1]);
+  });
+  return result;
+}
