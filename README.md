dbe9faf91869257e1a82330d61503660d69c1efa
Artjom Löbsack <ceigh@pm.me>
2019-07-03 16:14:10 +0500

    feat: Make auth user-friendly

--- README.md
+++ README.md
@@ -1,19 +1,3 @@
 # Otoha
 
 Parse vk database by keywords.
-
-# Setup
-
-First you need to install dependencies:
-
-`npm i`
-
-# Usage
-
-You can start browser version by 
-
-`npm run start`
-
-then open 
-
-`http://localhost:9000`
--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,48 @@
 import '../css/index.css';
 import cookie from './cookie';
+import vk     from './vk';
 
 // If change, don't forget to also change on vk.com/dev
 const baseUrl = 'http://localhost:9090';
-const authUrl = 'https://oauth.vk.com/authorize?client_id' +
-  `=7041394&display=page&scope=offline&redirect_uri=${baseUrl}&response_type=token`;
+const authUrl = 'https://oauth.vk.com/authorize?client_id=7041394&display=' +
+  `page&scope=offline&redirect_uri=${baseUrl}&response_type=token`;
 const url = location.href;
 const urlData = getJsonFromUrl(url.substr(url.search('#')));
-
+const storedQuery = cookie.get('query');
 const input = document.querySelector('#search-block input');
+
 const selectOnClick = el => {
   el.addEventListener('click', () => {
     el.focus();
     el.select();
   });
 };
-
 selectOnClick(input);
+
+if ('error' in urlData) location.replace(baseUrl);
 
 if ('access_token' in urlData) {
   cookie.set('token', urlData.access_token);
   location.replace(baseUrl);
+} else if (storedQuery) {
+  vk.init(cookie.get('token'));
+  vk.groups.search(storedQuery);
+  cookie.del('query');
 }
 
 input.onchange = () => {
+  const query = input.value;
+
+  if (!query) return;
+  if (storedQuery) cookie.del('query');
+
   const token = cookie.get('token');
 
   if (token) {
-    console.log(token);
+    vk.init(token);
+    vk.groups.search(query);
   } else {
+    cookie.set('query', query);
     location.replace(authUrl);
   }
 };
